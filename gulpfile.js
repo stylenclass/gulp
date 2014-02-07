f2691154fe2881cd41c02f2ad96d30394b04f93d
chris <deezahyn@gmail.com>
2014-02-07 15:03:52 +0800

    update package json

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,23 @@ var rev = require('gulp-rev');
 var uglify = require('gulp-uglify');
 
 gulp.task('clean', function() {
-    return gulp.src(['./index.html', 'js/min'], {read:false}).pipe(clean());
+    return gulp.src(['./index.html', 'assets/js/min'], {read:false}).pipe(clean());
 });
 
 gulp.task('html', ['clean'], function () {
   return gulp.src('./dev.html')
     .pipe(rename('index.html'))
     .pipe(usemin({ cssmin: false, htmlmin: false, jsmin: false }))
-    .pipe(context.replace(/src="js\/min\/(\w+\.js)"/g, 'src="js/min/{{$1}}"'))
+    .pipe(context.replace(/src="assets\/js\/min\/(\w+\.js)"/g, 'src="assets/js/min/{{$1}}"'))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('js', ['html'], function () {
-  return gulp.src(['js/min/app.js', 'js/min/foo.js']) // FRAGILE: gulp-usemin creates this file
+  return gulp.src(['assets/js/min/app.js', 'assets/js/min/foo.js']) // FRAGILE: gulp-usemin creates this file
     .pipe(ngmin())
     .pipe(uglify())
     .pipe(rev())
-    .pipe(gulp.dest('./js/min/'))
+    .pipe(gulp.dest('./assets/js/min/'))
 });
 
 gulp.task('default', ['js']);
